675c7b28ecdeb21520750ce002a73951be5b7621
Shailesh Mishra <Shailesh Mishra>
2017-02-03 22:52:32 +0545

    Second commit

---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/EndCriterion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCriterion : MonoBehaviour {
+
+    // Use this for initialization
+    private static int BallCount;
+    private static int score;
+    public static bool isRunning;
+    int missed;
+	void Start () {
+        BallCount = 0;
+        score = 0;
+        isRunning = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        IsRunning();
+        score = PopulateBall.score;
+        BallCount = PopulateBall.GetBallNumber;
+        missed = BallCount - score;
+        if (missed > 3)
+        {
+            Debug.Log("GAME OVER! GG NOOB!");
+            isRunning = false; 
+        }
+        
+    }
+    public void IsRunning()
+    {
+        isRunning = true;
+    }
+   
+}
---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/PopulateBall.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateBall : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject ball; // A gameobject to hold ball prefab!
+    GameObject balls;
+    public static int score;
+    public static float BallTimer; //Rate at which ball is being spawned. Initail BallTimer = 2f; 
+    public static float DestroyTime; //Destroy period of ball
+    public static int RandomLocation; // Position for random location
+    public static int GetBallNumber;
+     private float timestamp;
+    void Start()
+    {
+        BallTimer = 2f;
+        DestroyTime = 5f;
+        Debug.Log("Hello I'm ball");
+        RandomLocation = 4;
+        GetBallNumber = 0; 
+        //InvokeRepeating("spawnball", BallTimer , RepeaterTime);
+        timestamp = Time.time + BallTimer;
+              
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (EndCriterion.isRunning == true)
+        {
+            if (timestamp < Time.time)
+            {
+                spawnball();
+                timestamp += BallTimer;
+            }
+
+            getInput();
+            Destroy(balls, DestroyTime);
+        }
+        else
+        {
+           Stop();
+        }
+            
+
+        
+     }
+    void spawnball()
+    {
+        Vector3 position = new Vector3(UnityEngine.Random.Range(0, RandomLocation), UnityEngine.Random.Range(0, RandomLocation), UnityEngine.Random.Range(506, 506));
+        balls = Instantiate(ball, position, Quaternion.identity) as GameObject;
+        balls.gameObject.tag = "Retro";
+        GetBallNumber += 1;       
+            
+    }
+   public void Stop()
+    {
+
+        Debug.Log("Stop Called");      
+        Destroy(balls);
+        Destroy(ball);
+
+    }
+    public void getInput() {
+        //while (EndCriterion.isRunning)//Destroting ball on MouseClick.
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit))
+                    {
+
+                        if (hit.collider.gameObject.tag == "Retro")
+                        {
+                            Destroy(hit.collider.gameObject);
+                            Debug.Log("It was hit!!!");
+                            score++;
+                            Debug.Log("Score = " + score);
+
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
