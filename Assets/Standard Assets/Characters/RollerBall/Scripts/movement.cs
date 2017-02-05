3ed445e24e96b4db7508a562cc001bf9714f19d9
Suyogsng <suyogwaitforitsng@gmail.com>
2017-02-05 18:22:21 +0545

    ball sjakey shakey in development

---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	float limitXneg, limitYneg, limitXpos, limitYpos;
+	// Use this for initialization
+	void Start () {
+		limitXneg = gameObject.transform.position.x - 2;
+		limitYneg = gameObject.transform.position.y - 2;
+		limitXpos = gameObject.transform.position.x + 2;
+		limitYpos = gameObject.transform.position.y + 2;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Random.Range(0,2)==1){
+			
+		}else{
+			
+			}
+	}
+}
