f1ee55245a650fa45bcd29f7c576af8594903a18
Shailesh Mishra <Shailesh Mishra>
2017-02-06 10:26:48 +0545

    start menu! can't exit just start!

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    // Use this for initialization
+    public Button startText;
+	void Start () {
+        startText = startText.GetComponent<Button>(); 
+	}
+	
+	// Update is called once per frame
+	public void StartLevel()
+    {
+        Application.LoadLevel("level1");
+    }
+}
